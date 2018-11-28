cac689cbae975d46b4d22d855f21181217c6865f
Juan <jupegarnica@gmail.com>
2018-11-28 10:22:53 +0100

    logs do not show in production

--- src/app/logger.service.ts
+++ src/app/logger.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { environment } from "../environments/environment";
 
 @Injectable({
   providedIn: 'root'
@@ -6,7 +7,10 @@ import { Injectable } from '@angular/core';
 export class LoggerService {
 
  log(...msg: Array<any>) {
-   console.log( 'todos los logs:', ...msg)
+   if (!environment.production) {
+
+    console.log( 'todos los logs:', ...msg)
+   }
  }
 
   error(msg: any) {
